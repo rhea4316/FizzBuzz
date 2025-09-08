a8f35b277d78adf1170bed62de5355976c634681
Regina He <reginahe@Reginas-Laptop-2.local>
2025-09-08 12:19:31 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -34,3 +34,36 @@ class FizzBuzz {
         }
     }
 }
+
+
+class FizzBuzzWhile {
+
+    public static void main(String[] args) {
+
+        doFizzBuzz();
+    }
+
+    private static void doFizzBuzz() {
+        int i = 0;
+
+        while (i < 100) {
+
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+
+        } else {
+            System.out.println(i);
+
+        } i++;
+    }
+    }
+}
