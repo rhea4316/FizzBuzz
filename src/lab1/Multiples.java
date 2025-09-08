9efc6f343ae763c55763ac530efda00e0f8745bf
Regina He <reginahe@reginaslaptop2.home>
2025-09-08 16:59:07 -0400

    new multiples method

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,24 @@
+package lab1;
+
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        System.out.println(multiples(10, 3, 3));
+    }
+
+    static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++){
+
+            boolean divisibleByA = i % a == 0;
+            boolean divisibleByB = i % b == 0;
+
+            if (divisibleByA || divisibleByB) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
